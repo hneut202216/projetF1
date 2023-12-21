bd799fe4918a88d42c36bd4a6fd75f861e21dd3c
hneut202216 <h.neut@students.ephec.be>
2023-12-21 10:00:40 +0100

    structure pilote et course

---
+++ structure.h
@@ -0,0 +1,27 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+struct Course
+{
+    int num;
+    char ville[100];
+    char pays[100];
+    char circuit[200];
+    char taille[10];
+    char gp[200];
+    char date[100];
+};
+
+struct Pilote
+{
+    int num;
+    char prenom[50];
+    char nom[50];
+    char team[50];
+    char bday[50];
+    char nation[50];
+    int temps;
+    int tempsTour[3];
+};
+
+#endif
