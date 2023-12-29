5a2786659be334cd8d401b29dad855b96e4b54f6
hneut202216 <h.neut@students.ephec.be>
2023-12-29 20:13:59 +0100

    pas performant

--- main.c
+++ main.c
@@ -71,6 +71,8 @@ int main(int argc, char **argv) {
         strcpy(listeFinale[indice].gp, strtok(NULL, ";"));
         strtok(NULL, ";");
         strcpy(listeFinale[indice].date, strtok(NULL, ";"));
+    
+        listeFinale[indice].nbrTours = atoi(strtok(NULL, ";"));
         afficheCourse(listeFinale[indice]);
         indice++;
 
@@ -124,33 +126,142 @@ int main(int argc, char **argv) {
     int tempsEnMoins = 0;
     int choix;
     int jqualif =0;
-    printf("choisissez un chiffre entre :\n1 : pour commencer avec la quallification 1\n2 : pour commencer avec la quallification 2\n3 : pour commencer avec la quallification 3\n4 : pour commencer la course\n Mettez un autre chiffre si vous voulez Quitter.\nVotre choix : ");
+    int bessaie = 0;
+    printf("choisissez un chiffre entre :\n1 : pour commencer l'essaie 1\n2 : pour commencer l'essaie 2\n3 : pour commencer l'essaie 3\n4 : pour commencer avec la quallification 1\n5 : pour commencer avec la quallification 2\n6 : pour commencer avec la quallification 3\n7 : pour commencer la course\n Mettez un autre chiffre si vous voulez Quitter.\nVotre choix : ");
     scanf("%d",&choix);
     switch(choix){
         case 1:
             nombreQ = 0;
             tempsEnMoins =0;
             jqualif =0;
+            bessaie = 0;
             break;
         case 2:
+            nombreQ = 0;
+            tempsEnMoins =0;
+            jqualif =0;
+            bessaie= 1;
+            break;
+        case 3:
+            nombreQ = 0;
+            tempsEnMoins =0;
+            jqualif =0;
+            bessaie = 2;
+            break;
+        case 4:
+            nombreQ = 0;
+            tempsEnMoins =0;
+            jqualif =0;
+            bessaie = 3;
+            break;
+        case 5:
             nombreQ = 5;
             tempsEnMoins =2;
             jqualif =1;
+            bessaie = 3;
             break;
-        case 3:
+        case 6:
             nombreQ = 10;
             tempsEnMoins =4;
             jqualif =2;
+            bessaie = 3;
             break;
-        case 4 :
+        case 7 :
             jqualif = 3;
+            bessaie = 3;
             break;
         default:
             return -1;
-
-
     }
-    //int accessQ = 0;
+       //int accessQ = 0;
+    for(int b = bessaie; b<3;b++){
+        //accessQ = 0;
+            for(int k = 0; k < 20 ; k++){
+                        listePilotes[k].temps = 0;
+                        listePilotes[k].tempsTour[0] = 0;
+                        listePilotes[k].tempsTour[1] = 0;
+                        listePilotes[k].tempsTour[2] = 0;
+
+            }
+        
+        for (int o = 0; o < 10 - tempsEnMoins; o++)
+        {
+            for (int i = 0; i < 20 ; i++){
+            // sleep(10);
+            int totalF = 0;
+            int a = 0;
+            usleep(200000);
+            p_id = fork();
+            if (p_id == 0){
+                srand(time(NULL)*getpid());
+                sem_wait(&semaphore);
+                while (a < 3){ 
+                int random = rand() % 20001;
+                int finaleR = 25000 + random;
+                totalF += finaleR;
+                
+                if (listePilotes[21].tempsTour[a] > finaleR || listePilotes[21].tempsTour[a] == 0){listePilotes[21].tempsTour[a] = finaleR;}
+                if (listePilotes[i].tempsTour[a] > finaleR || listePilotes[i].tempsTour[a] == 0){listePilotes[i].tempsTour[a] = finaleR;}
+                //printf("%d ms        ",finaleR);
+                a++;
+
+                
+                
+                }
+                if(listePilotes[21].temps > totalF || listePilotes[21].temps == 0){listePilotes[21].temps = totalF;}
+                if(listePilotes[i].temps > totalF || listePilotes[i].temps == 0){listePilotes[i].temps = totalF;}
+                sem_post(&semaphore);
+                return 0;
+            }
+
+            else if (p_id < 0)
+            {
+                printf("pas bon erreur");
+                return -1;
+            }
+            else
+            {
+                 sem_wait(&semaphore);
+
+                sem_post(&semaphore);
+                
+            }
+        while(p_id == waitpid(-1,NULL,0)){
+            if(errno == ECHILD){
+                break;
+            }
+        }
+        qsort(listePilotes, 20, sizeof(struct Pilote), compare);
+        afficherDonnees(listePilotes, 20,b);
+        
+        }
+        //printf("Qualif : %d",j+1);
+        }
+        if(nombreQ < 10){nombreQ+=5;}
+        tempsEnMoins +=2;
+        afficheMeilleurTemps(listePilotes,2);
+        /*while(accessQ != 1){
+            
+            printf("écrivez 1 si vous voulez terminer la qualification %d :",j+1);
+            scanf("%d",&accessQ);
+
+            }*/
+            
+            listePilotes[21].temps = 0;
+            listePilotes[21].tempsTour[0] = 0;
+            listePilotes[21].tempsTour[1] = 0;
+            listePilotes[21].tempsTour[2] = 0;
+            sleep(4);
+            if(b>2){
+                nombreQ = 0;
+                tempsEnMoins =0;
+                jqualif =0;
+                afficherDonnees(listePilotes,20,b);
+                printf("êtes vous prét pour les qualification?(appuyer sur Enter , ctrl + c pour quitter)");
+                scanf("%lc",&choix);
+                }
+    }
+    
     for(int j = jqualif; j<3;j++){
         //accessQ = 0;
             for(int k = 0; k < 15-nombreQ ; k++){
@@ -167,7 +278,7 @@ int main(int argc, char **argv) {
             // sleep(10);
             int totalF = 0;
             int a = 0;
-            //usleep(1000000);
+            usleep(200000);
             p_id = fork();
             if (p_id == 0){
                 srand(time(NULL)*getpid());
@@ -209,7 +320,7 @@ int main(int argc, char **argv) {
             }
         }
         qsort(listePilotes, 20-nombreQ, sizeof(struct Pilote), compare);
-        afficherDonnees(listePilotes, 20-nombreQ);
+        afficherDonnees(listePilotes, 20-nombreQ,j+3);
         
         }
         //printf("Qualif : %d",j+1);
@@ -230,7 +341,7 @@ int main(int argc, char **argv) {
             listePilotes[21].tempsTour[2] = 0;
             sleep(4);
     }
-   afficherDonnees(listePilotes,20);
+   afficherDonnees(listePilotes,20,6);
    printf("Qualif terminé\n");
    /*printf("écrivez 1 si vous voulez terminer la qualification la course:");
             scanf("%d",&accessQ);
@@ -244,7 +355,7 @@ int main(int argc, char **argv) {
                 // sleep(10);
                 int totalF = 0;
                 int a = 0;
-                //usleep(1000000);
+                usleep(200000);
                 p_id = fork();
                 if (p_id == 0){
                     srand(time(NULL)*getpid());
@@ -260,6 +371,7 @@ int main(int argc, char **argv) {
                     a++;
                     }
                     if(listePilotes[21].temps > totalF || listePilotes[21].temps == 0){listePilotes[21].temps = totalF;}
+                    if(listePilotes[i].meilleurTemps > totalF || listePilotes[i].meilleurTemps== 0){listePilotes[i].meilleurTemps = totalF;}
                     listePilotes[i].temps += totalF;
                     listePilotes[i].nbrTour += 1;
                     sem_post(&semaphore);
@@ -280,13 +392,36 @@ int main(int argc, char **argv) {
                     break;
                 }
             }
+            afficherDonnees(listePilotes, 20,6);
             
-            
+            afficheMeilleurTemps(listePilotes, 2);
             }
             qsort(listePilotes, 20, sizeof(struct Pilote), compare);
-            afficherDonnees(listePilotes, 20);
+            afficherDonnees(listePilotes, 20,6);
+            
             afficheMeilleurTemps(listePilotes, 2);
+            affichePoint(listePilotes,20);
+            
         }
+        
+        for(int i = 0 ; i<20 ;i++){
+
+            
+            printf("coucou");
+            if(i = 0){listePilotes[i].point += 25;}
+            if(i = 1){listePilotes[i].point += 18;}
+            if(i = 2){listePilotes[i].point += 15;}
+            if(i = 3){listePilotes[i].point += 12;}
+            if(i = 4){listePilotes[i].point += 10;}
+            if(i = 5){listePilotes[i].point += 8;}
+            if(i = 6){listePilotes[i].point += 6;}
+            if(i = 7){listePilotes[i].point += 4;}
+            if(i = 8){listePilotes[i].point += 2;}
+            if(i = 9){listePilotes[i].point += 1;}
+            //if((i <= 9 )&& (listePilotes[21].temps = listePilotes[i].temps)){listePilotes[i].point += 1;}
+            if((i>9)){listePilotes[i].point += 0;}
+        }
+        affichePoint(listePilotes,20);
 
    sem_destroy(&semaphore);
    shmdt(listePilotes);
